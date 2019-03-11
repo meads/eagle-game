deebf9436696e4a34e75d5c34cb269c8431b7f39
Mike Eads <me@mikeads.com>
2019-03-10 18:25:53 -0700

    exploding cats

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
     var mid
     var fore
     var app
+    var sprites = []
+    const bottom = 350
 
     function Eagle (app) {
         this.anim = null
@@ -14,8 +16,11 @@
         this.stepDistance = 10
         this.direction = {up:false, down:false,left:false,right:false}
         this.items = {}
+        this.container = new PIXI.Container()
         this.createAnimatedSprite(app)
         this.bindKeyboardControls()
+        
+        return this
     }
     Eagle.prototype.uuidv4 = function() {
         return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
@@ -26,6 +31,8 @@
         if (!this.items[item.name]) {
             item.name = this.uuidv4()
             this.items[item.name] = item
+            this.container.addChild(item)
+            console.log(this.container.children)
         }
     }
     Eagle.prototype.bindKeyboardControls = function() {
@@ -45,6 +52,16 @@
         arrowRight.press = () => this.direction.right = true
         arrowRight.release = () => this.direction.right = false            
 
+        let spaceBar = keyboard(" ")
+        spaceBar.press = () => {
+            let offset = 2
+            this.container.children.forEach((val, i, arr) => {
+                offset += 10
+                sprites.push(new FallingCat(this.anim.position.x+offset, this.anim.position.y))
+            })
+            this.container.removeChildren()
+        }
+        spaceBar.release = () => {}
     }
     Eagle.prototype.createAnimatedSprite = function(app) {
         var frames = []
@@ -57,7 +74,9 @@
         this.anim.anchor.set(0.5)
         this.anim.animationSpeed = 0.25
         this.anim.play()
-        app.stage.addChild(this.anim)
+
+        return this
+        // app.stage.addChild(this.anim)
     }
     Eagle.prototype.update = function() {
         if (this.direction.up) {
@@ -110,7 +129,9 @@
         this.anim.anchor.set(0.5)
         this.anim.animationSpeed = 0.25
         this.anim.play()
-        app.stage.addChild(this.anim)
+
+        return this
+        // app.stage.addChild(this.anim)
     }
     Cat.prototype.update = function() {
         if (this.anim.position.x-5 <= 0) {
@@ -120,6 +141,70 @@
             this.anim.position.x -= 5
         }
     }
+
+    function FallingCat(x, y) {
+        this.removed = false
+        this.anim = null
+        this.createAnimatedSprite(x, y)
+    }
+    FallingCat.prototype.createAnimatedSprite = function(x, y) {
+        var frames = []
+        for (var i = 1; i < 4; i++)
+            frames.push(PIXI.Texture.fromFrame('catfwd_0' + i + '.png'))
+
+        this.anim = new PIXI.extras.AnimatedSprite(frames)
+        this.anim.x = x
+        this.anim.y = y
+        this.anim.anchor.set(0.5)
+        this.anim.animationSpeed = 0.25
+        this.anim.play()
+
+        this.add()
+    }
+    FallingCat.prototype.add = function() {
+        app.stage.addChild(this.anim)        
+    }
+    FallingCat.prototype.remove = function() {
+        this.removed = true
+        app.stage.removeChild(this.anim)
+    }
+    FallingCat.prototype.update = function() {
+        if (this.anim.position.y+5 < bottom) {
+            this.anim.position.y += 5
+        } else if (!this.removed) {
+            new Explosion(this.anim.position.x, this.anim.position.y)
+            this.remove()
+        }
+    }
+
+    function Explosion(x, y) {
+        this.anim = null
+        this.timeout = null
+        this.destroyAfter = 200
+        this.createAnimatedSprite(x, y)
+    }
+    Explosion.prototype.createAnimatedSprite = function(x, y) {
+        var frames = []
+        for (var i = 1; i < 9; i++) {
+            frames.push(PIXI.Texture.fromFrame('tile00' + i + '.png'))
+        }
+        this.anim = new PIXI.extras.AnimatedSprite(frames)
+        this.anim.x = x
+        this.anim.y = bottom-32
+        this.anim.anchor.set(0.5)
+        this.anim.animationSpeed = 0.5
+        this.anim.play()
+        this.add()
+    }
+    Explosion.prototype.add = function() {
+        app.stage.addChild(this.anim)
+        this.timeout = setTimeout(() => {this.remove()}, this.destroyAfter)
+    }
+    Explosion.prototype.remove = function() {
+        app.stage.removeChild(this.anim)
+        clearTimeout(this.timeout)
+    }
+    Explosion.prototype.update = function() {}
 
     // Setup Pixi and load the texture atlas files - call the `setup` function when they've loaded
     PIXI.loader.add("resources/eagledata.json")
@@ -176,9 +261,18 @@
         app.stage.addChild(fore)
 
         eagle = new Eagle(app)
-        cats = [new Cat(app)]
+        app.stage.addChild(eagle.anim)
+        app.stage.addChild(eagle.container)
 
-        setInterval(() => cats.push(new Cat(app)), 1500)
+        let cat = new Cat(app)
+        cats = [cat]
+        app.stage.addChild(cat.anim)
+
+        setInterval(() => { 
+            let c = new Cat(app)
+            cats.push(c)
+            app.stage.addChild(c.anim)
+        }, 1500)
 
         gameState = play
 
@@ -205,10 +299,14 @@
                     // return
                 // }
                 cats.splice(cats.indexOf(c), 1)
-                // c.direction = eagle.direction
                 eagle.addItem(c.anim)
             }
         })
+
+        sprites.forEach(c => {
+            c.update()
+        })
+
         // parallax scrollng layers
         far.tilePosition.x -= 0.128
         mid.tilePosition.x -= 0.64
